1d0ddc9784a469671b90cd82757c5237db75d29e
Parinthorn Kanurak <parinthorn112545@gmail.com>
2021-09-03 21:47:45 +0700

    change

---
+++ week 5 method 2/Source.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int factorial(int z);
+int main() {
+	int x,y;
+	printf("enter positive number: ");
+	scanf("%d",&x);
+	factorial(x);
+	y = factorial(x);
+	printf("factorial of %d is %d", x, y);
+	return 0;
+}
+int factorial(int z)
+{
+	if (z==0) {
+		return 1;
+	}
+		return z * factorial(z-1);
+	
+}
